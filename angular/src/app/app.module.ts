9a15b7b1e5c233ed141391eb5bd3a113594b1d7f
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2023-01-03 15:48:43 +0700

    Embed Sakai_ng template

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { registerLocale } from '@abp/ng.core/locale';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
-import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
-import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
-import { ThemeSharedModule } from '@abp/ng.theme.shared';
+// import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
+// import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
+// import { ThemeSharedModule } from '@abp/ng.theme.shared';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from './layout/app.layout.module';
 import { APP_ROUTE_PROVIDER } from './route.provider';
 
 @NgModule({
@@ -20,17 +21,18 @@ import { APP_ROUTE_PROVIDER } from './route.provider';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    AppLayoutModule,
     CoreModule.forRoot({
       environment,
       registerLocaleFn: registerLocale(),
     }),
-    ThemeSharedModule.forRoot(),
+    //ThemeSharedModule.forRoot(),
     AccountConfigModule.forRoot(),
     IdentityConfigModule.forRoot(),
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
-    ThemeLeptonXModule.forRoot(),
-    SideMenuLayoutModule.forRoot(),
+    // ThemeLeptonXModule.forRoot(),
+    // SideMenuLayoutModule.forRoot(),
   ],
   declarations: [AppComponent],
   providers: [APP_ROUTE_PROVIDER],

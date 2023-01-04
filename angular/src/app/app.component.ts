1875a3e3f5264ff21898c984e06b0d064863a602
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2023-01-04 10:18:33 +0700

    create module login

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   template: `
-    <router-outlet></router-outlet> 
+    <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  menuMode = 'static';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    document.documentElement.style.fontSize = '14px';
+  }
+}
